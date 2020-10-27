a50d50fa49cf5c0d742710920106c64c433db0f6
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-10-27 13:17:38 +0530

    added loading spinner

--- dotnet-core/Controllers/Controller.cs
+++ dotnet-core/Controllers/Controller.cs
@@ -30,10 +30,9 @@ namespace dotnet_core.Controllers
         } 
        
         
-       // GET api/commands/{id}        
+        // GET api/commands/{id}        
         [HttpGet("{id}", Name="GetPostById")]
-        public async Task<IActionResult> GetPostById(int id)
-        {
+        public async Task<IActionResult> GetPostById(int id){
              ServiceResponse<Post> x=await _repository.GetPostByIdDB(id);
             if(x.Success)
               return Ok(x);  
@@ -43,18 +42,18 @@ namespace dotnet_core.Controllers
        
  
    
-    [HttpPost]
-    public async Task<IActionResult> Add(Post cmd){
-       
-            var ret=await _repository.AddPostDB(cmd); 
-            Console.WriteLine(cmd) ;                     
-            return Ok(ret);            
-        }
-        
+        [HttpPost]
+        public async Task<IActionResult> Add(Post cmd){
+           
+                var ret=await _repository.AddPostDB(cmd); 
+                Console.WriteLine(cmd) ;                     
+                return Ok(ret);            
+            }
+            
 
-   
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Update(Post cmd,int id){       
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Post cmd,int id){       
             var ret=await _repository.UpdatePostDB(cmd,id);        
             if(!ret.Success){
                return NotFound(ret);
@@ -62,13 +61,11 @@ namespace dotnet_core.Controllers
             return Ok(ret);            
         } 
 
-    [HttpDelete("{id}")]
-    
-    public async Task<IActionResult> Delete(int id)
-        {
-            ServiceResponse<List<Post>> response = await _repository.DeleteDB(id);
-            return Ok(response);
+        [HttpDelete("{id}")]    
+        public async Task<IActionResult> Delete(int id){
+                ServiceResponse<List<Post>> response = await _repository.DeleteDB(id);
+                return Ok(response);
         }         
-    
+        
   }
 }
