89cefd85c3bcdcf3dac823939631058d852b4b25
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-10-30 19:13:48 +0530

    ok

--- dotnet-core/Data/IPost.cs
+++ dotnet-core/Data/IPost.cs
@@ -7,19 +7,19 @@ namespace dotnet_core.Data
 {
     public interface IPost
     {                 
-      Task<ServiceResponse<List<Post>>> Getter();
+      //Task<ServiceResponse<List<Post>>> Getter();
       Task<ServiceResponse<List<Post>>> GetterDB();
 
-      Task<ServiceResponse<Post>> GetPostById(int id);
+      //Task<ServiceResponse<Post>> GetPostById(int id);
       Task<ServiceResponse<Post>> GetPostByIdDB(int id);
 
-      Task<ServiceResponse<List<Post>>> AddPost(Post data);
+      //Task<ServiceResponse<List<Post>>> AddPost(Post data);
       Task<ServiceResponse<List<Post>>> CreatePostDB(Post data);
 
-      Task<ServiceResponse<List<Post>>> UpdatePost( Post model, int id);
+     //Task<ServiceResponse<List<Post>>> UpdatePost( Post model, int id);
       Task<ServiceResponse<Post>> UpdatePostDB(Post data,int id);
 
-      Task<ServiceResponse<List<Post>>> Delete(int id);
+      //Task<ServiceResponse<List<Post>>> Delete(int id);
       Task<ServiceResponse<List<Post>>> DeleteDB(int id);
      
     }
