5c0115d6010799b838962d6293c902c27d76e7a6
Somsubhra97 <somsubhrablasterjaxxmitra@gmail.com>
2020-11-04 11:22:16 +0530

    ok

--- dotnet-core/Model/Post.cs
+++ dotnet-core/Model/Post.cs
@@ -1,16 +1,19 @@
+usig System.ComponentModel.DataAnnotations;
 //using System.ComponentModel.DataAnnotations;
 
 namespace dotnet_core.Models
 {
     public class Post
     {
-        
+        [Key]
         public int id { get; set; }
 
-       
+        [Required]
+        [Column(TypeName="nvarchar(100)")]
         public string content { get; set; }
         
-        
+        [Required]
+        [Column(TypeName="nvarchar(100)")]
         public string title { get; set; }        
        
         
